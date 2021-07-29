82a0a8eff07092370fb97b11433e857308eeb07b
Ibrar-Khatri <khatriibrar116@gmail.com>
2021-07-29 13:17:00 -0700

    gitignore added in server

---
+++ OPS-Server/app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const session = require('express-session')
+const port = 3000
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+
+app.use(session({
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+    secret: 'keyboard cat'
+}));
+
+app.get('/', function (req, res) {
+    console.log('Request' + JSON.stringify(req))
+    var body = '';
+    if (req.session.views) {
+        ++req.session.views;
+    } else {
+        req.session.views = 1;
+        body += '<p>First time visiting? view this page in several browsers :)</p>';
+    }
+    res.send(body + '<p>viewed <strong>' + req.session.views + '</strong> times.</p>');
+});
+
+app.listen(port, () => {
+    console.log(`Server Started Successfully`)
+})
