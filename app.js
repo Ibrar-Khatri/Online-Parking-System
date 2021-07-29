16fd278d5a2fb46276f594a022a3a7ad06d2a83d
Ibrar-Khatri <khatriibrar116@gmail.com>
2021-07-29 14:47:28 -0700

    checking server

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const session = require('express-session')
-const port = 3000
+const port = process.env.PORT
 
 app.get("*", (req, res) => {
     res.send("<h1>Welcome to OLX Server</h1>");
