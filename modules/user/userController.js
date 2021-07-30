9c58c1ed5acaddf9812ce46b7f79c252f0e1b344
Ibrar-Khatri <khatriibrar116@gmail.com>
2021-07-29 18:04:28 -0700

    working on authentication

--- modules/user/userController.js
+++ modules/user/userController.js
@@ -1,19 +1,17 @@
-// import firebase from '../../firebase/firebase'
 const firebase = require('../../firebase/firebase')
 
 module.exports.signupWithDetails = (req, res) => {
-    console.log(req.body.email, "email")
-    // console.log(req.password + "password")
-    firebase.auth().createUserWithEmailAndPassword(req.email, req.password)
-        .then(user => {
-            console.log(user)
-            res.send({
-                status: 'true', user: user
-            })
-        })
-        .catch(err => {
-            res.send({
-                status: 'false',
-            })
-        })
+    console.log(req, "email")
+    // firebase.auth().createUserWithEmailAndPassword(req.email, req.password)
+    //     .then(user => {
+    //         console.log(user)
+    //         res.send({
+    //             status: 'true', user: user
+    //         })
+    //     })
+    //     .catch(err => {
+    //         res.send({
+    //             status: 'false',
+    //         })
+    //     })
 }
