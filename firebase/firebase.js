00f44674b60121da20904fd89dd73f16e16b8fd1
Ibrar-Khatri <khatriibrar116@gmail.com>
2021-07-29 17:35:28 -0700

    working on authentication

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,20 +1,33 @@
 // import firebase from "firebase";
 const firebase = require('firebase')
 
-var firebaseConfig = {
-    apiKey: "AIzaSyCzIRib2wuOFbq6EbGrEKksZb1qP80JX64",
-    authDomain: "online-parking-system-8e146.firebaseapp.com",
-    projectId: "online-parking-system-8e146",
-    storageBucket: "online-parking-system-8e146.appspot.com",
-    messagingSenderId: "510657373183",
-    appId: "1:510657373183:web:18730d7720723473257dca",
-    measurementId: "G-M3G34TGQD9"
-};
+// var firebaseConfig = {
+//     apiKey: "AIzaSyCzIRib2wuOFbq6EbGrEKksZb1qP80JX64",
+//     authDomain: "online-parking-system-8e146.firebaseapp.com",
+//     projectId: "online-parking-system-8e146",
+//     storageBucket: "online-parking-system-8e146.appspot.com",
+//     messagingSenderId: "510657373183",
+//     appId: "1:510657373183:web:18730d7720723473257dca",
+//     measurementId: "G-M3G34TGQD9"
+// };
+
+var firebaseConfig = firebase.initializeApp(
+    {
+        apiKey: "AIzaSyCzIRib2wuOFbq6EbGrEKksZb1qP80JX64",
+        authDomain: "online-parking-system-8e146.firebaseapp.com",
+        projectId: "online-parking-system-8e146",
+        storageBucket: "online-parking-system-8e146.appspot.com",
+        messagingSenderId: "510657373183",
+        appId: "1:510657373183:web:18730d7720723473257dca",
+        measurementId: "G-M3G34TGQD9"
+    }
+)
 
 
-firebase.initializeApp(firebaseConfig);
+// var firebase
+// firebase.initializeApp(firebaseConfig);
 
-firebase.analytics();
+// firebase.analytics();
 
 
-module.exports = firebase
+module.exports = firebaseConfig;
