944eebb289cee850f8e13c270194ff5b5f30e554
Ibrar-Khatri <khatriibrar116@gmail.com>
2021-07-29 18:15:43 -0700

    adding body parser

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const session = require('express-session')
-const port = process.env.PORT;
 const userRoute = require('./modules/user/userRoute')
+const port = process.env.PORT;
 
 app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("*", (req, res) => {
     res.send("<h1>Welcome to online parking system</h1>");
