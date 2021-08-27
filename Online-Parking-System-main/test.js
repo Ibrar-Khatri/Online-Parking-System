e8da667fea06775899a7736aff327a6f36dcdf3a
Ibrar-Khatri <khatriibrar116@gmail.com>
2021-08-28 01:57:19 +0500

    prjects up in ios and android  mobile

---
+++ Online-Parking-System-main/test.js
@@ -0,0 +1,16 @@
+let bookings = {
+  gVFZQ4m8is9b0Z5XbSo3: {
+    startTime: '',
+    endTime: '',
+    userId: '',
+    slotName: '',
+    nameOfLocation: '',
+  },
+  gVFZQ4m8is9b0Z5X23qwd: {
+    startTime: '',
+    endTime: '',
+    userId: '',
+    slotName: '',
+    nameOfLocation: '',
+  },
+};
